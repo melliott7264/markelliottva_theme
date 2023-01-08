90986007aa3a979d87115bff9abb70d64faa5e89
Mark Elliott <markelliottva@gmail.com>
2023-01-08 12:52:17 -0500

    cleanup functions.php

--- functions.php
+++ functions.php
@@ -17,25 +17,5 @@ add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style( 'style', get_stylesheet_uri() ); // use style.css from theme folder
 });
 
-/** 
- *  Register new menu
-*/
-// register_nav_menus( array(
-//     'primary' => __( 'MarkElliott2', 'markelliottva' ),
-// ) );
 
-// if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
-//     // File does not exist... return an error.
-//     return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
-// } else {
-//     // File exists... require it.
-//     require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
-// }
 
-/**
- * Register Custom Navigation Walker
- */
-// function register_navwalker(){
-// 	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
-// }
-// add_action( 'after_setup_theme', 'register_navwalker' );
